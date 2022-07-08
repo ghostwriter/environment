49b36b7c9e92fee9ff648b2eee943a4479f80965
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-07 19:18:24 -0500

    Create VariableTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/VariableTest.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Environment\Tests\Unit;
+
+use Ghostwriter\Environment\Contract\VariableInterface;
+use Ghostwriter\Environment\Exception\InvalidVariableNameException;
+use Ghostwriter\Environment\Exception\InvalidVariableValueException;
+use Ghostwriter\Environment\Variable;
+
+/**
+ * @coversDefaultClass \Ghostwriter\Environment\Variable
+ *
+ * @internal
+ *
+ * @small
+ */
+final class VariableTest extends AbstractTestCase
+{
+    private VariableInterface $variable;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->variable = new Variable('NAME', 'V4lu3');
+    }
+
+    /**
+     * @return iterable<string,array<int,string>>
+     */
+    public function environmentVariablesProvider(): iterable
+    {
+        yield from [
+            'default' => ['name', 'value'],
+            'empty-name' => ['', 'value'],
+            'empty-value' => ['name', ''],
+            'untrimmed-name' => [' name ', 'value'],
+            'untrimmed-value' => ['name', ' value '],
+            'name-contains-equal-sign' => ['na=me', 'value'],
+            'value-contains-equal-sign' => ['name', 'val=ue'],
+            'name-contains-NULL-byte' => ["na\0me", 'value'],
+            'value-contains-NULL-byte' => ['name', "val\0ue"],
+        ];
+    }
+
+    /**
+     * @covers \Ghostwriter\Environment\Variable::__construct
+     * @covers \Ghostwriter\Environment\Variable::assertValidVariableName
+     * @covers \Ghostwriter\Environment\Variable::assertValidVariableValue
+     * @covers \Ghostwriter\Environment\Variable::getName
+     * @covers \Ghostwriter\Environment\Variable::getValue
+     * @dataProvider environmentVariablesProvider
+     */
+    public function testConstruct(string $name, string $value): void
+    {
+        $trimmedName = trim($name);
+        if (
+            '' === $trimmedName ||
+            $name !== $trimmedName ||
+            str_contains($name, '=') ||
+            str_contains($name, "\0")
+        ) {
+            $this->expectException(InvalidVariableNameException::class);
+        }
+
+        $trimmedValue = trim($value);
+        if (
+            '' === $trimmedValue ||
+            $value !== $trimmedValue ||
+            str_contains($value, "\0")
+        ) {
+            $this->expectException(InvalidVariableValueException::class);
+        }
+
+        self::assertSame($name, (new Variable($name, $value))->getName());
+        self::assertSame($value, (new Variable($name, $value))->getValue());
+    }
+
+    /**
+     * @covers \Ghostwriter\Environment\Variable::__construct
+     * @covers \Ghostwriter\Environment\Variable::assertValidVariableName
+     * @covers \Ghostwriter\Environment\Variable::assertValidVariableValue
+     * @covers \Ghostwriter\Environment\Variable::getName
+     */
+    public function testGetName(): void
+    {
+        self::assertSame('NAME', $this->variable->getName());
+    }
+
+    /**
+     * @covers \Ghostwriter\Environment\Variable::__construct
+     * @covers \Ghostwriter\Environment\Variable::assertValidVariableName
+     * @covers \Ghostwriter\Environment\Variable::assertValidVariableValue
+     * @covers \Ghostwriter\Environment\Variable::getValue
+     */
+    public function testGetValue(): void
+    {
+        self::assertSame('V4lu3', $this->variable->getValue());
+    }
+}

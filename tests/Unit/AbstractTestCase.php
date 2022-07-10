e5d760a85bdccd1df1f3008ca87fb60680848293
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-10 04:43:05 -0500

    Update AbstractTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/AbstractTestCase.php
+++ tests/Unit/AbstractTestCase.php
@@ -8,8 +8,29 @@ use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTestCase extends TestCase
 {
+    /**
+     * @var array<string,string>
+     */
+    protected array $backupEnvironmentVariables = [];
+
+    /**
+     * @var array<string,string>
+     */
+    protected array $backupServerVariables = [];
+
     protected function setUp(): void
     {
         parent::setUp();
+        /** @var array<string,string> $this->backupEnvironmentVariables */
+        $this->backupEnvironmentVariables = getenv() ?: $_ENV;
+        /** @var array<string,string> $this->backupServerVariables */
+        $this->backupServerVariables = $_SERVER;
+    }
+
+    protected function tearDown(): void
+    {
+        $_ENV = $this->backupEnvironmentVariables;
+        $_SERVER = $this->backupServerVariables;
+        parent::tearDown();
     }
 }

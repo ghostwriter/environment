9b214aabc1d2e0e4fe897e387a920781bc6fa7d8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-12 22:58:15 -0500

    Update VariableInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/VariableInterface.php
+++ src/Contract/VariableInterface.php
@@ -7,13 +7,6 @@ namespace Ghostwriter\Environment\Contract;
 interface VariableInterface
 {
     /**
-     * Get the variable as an array ['name'=>'value'].
-     *
-     * @return array<string,string>
-     */
-    public function toArray(): array;
-
-    /**
      * Get the variable name.
      */
     public function getName(): string;
@@ -22,4 +15,11 @@ interface VariableInterface
      * Get the variable value.
      */
     public function getValue(): string;
+
+    /**
+     * Get the variable as an array ['name'=>'value'].
+     *
+     * @return array<string,string>
+     */
+    public function toArray(): array;
 }

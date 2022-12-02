2e18d3d6e8957ca72ab8a322fe2dee981216f67f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-02 14:55:00 -0600

    Update ServerVariable.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServerVariable.php
+++ src/ServerVariable.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Ghostwriter\Environment;
 
 use Ghostwriter\Environment\Contract\ServerVariableInterface;
+use Ghostwriter\Environment\Traits\VariableTrait;
 
 /**
  * @see \Ghostwriter\Environment\Tests\Unit\ServerVariableTest
  */
-final class ServerVariable extends AbstractVariable implements ServerVariableInterface
+final class ServerVariable implements ServerVariableInterface
 {
+    use VariableTrait;
 }

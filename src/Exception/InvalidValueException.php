cf167e647de4431f5dad11417cd137fe18efe529
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-21 20:43:32 -0500

    Update InvalidValueException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InvalidValueException.php
+++ src/Exception/InvalidValueException.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Environment\Exception;
 
+use Ghostwriter\Environment\Exception;
 use RuntimeException;
 
-final class InvalidValueException extends RuntimeException implements EnvironmentExceptionInterface
+final class InvalidValueException extends RuntimeException implements Exception
 {
 }

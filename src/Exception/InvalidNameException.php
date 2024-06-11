c8f2b5f74846cb37108b00ecc4fa036e43f24b77
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 17:13:09 -0500

    Update InvalidNameException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InvalidNameException.php
+++ src/Exception/InvalidNameException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Environment\Exception;
 use Ghostwriter\Environment\Exception;
 use RuntimeException;
 
-final class InvalidNameException extends RuntimeException implements Exception {}
+final class InvalidNameException extends RuntimeException implements Exception
+{
+}
